04a3f7b15eb22a02fe990d11a23730f83c6fc23e
03bgls25 <03bgls25@gmail.com>
2018-08-21 16:38:11 +0545

    integrated project with bower for front end development

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,45 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var sourcemaps = require('gulp-sourcemaps');
-var autoprefixer = require('gulp-autoprefixer');
-var minifycss = require('gulp-minify-css');
-var rename = require('gulp-rename');
-var plumber = require('gulp-plumber');
-var notify = require("gulp-notify");
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    less = require('gulp-less'),
+    jshint = require('gulp-jshint'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    sourcemaps = require('gulp-sourcemaps'),
+    autoprefixer = require('gulp-autoprefixer'),
+    minifycss = require('gulp-minify-css'),
+    rename = require('gulp-rename'),
+    plumber = require('gulp-plumber'),
+    notify = require('gulp-notify'),
+    browserSync = require('browser-sync').create();
 
 var config = {
-    scssPath : './scss/',
-    dest : './public_html/'
+    paths: {
+        src: {
+            scss: './src/scss/',
+            less: './src/less/',
+            scripts: './src/scripts/',
+        },
+        dest: './docs/',
+    },
+    serve: {
+        proxy: '',
+        server: './docs/'
+    }
 };
 
 gulp.task('sass', function() {
-    var onError = function(err) {
-        notify.onError({
-            title: "Gulp Sass",
-            subtitle: "Failure",
-            message: "<%= error.message %>",
-            sound: "Beep"
-        })(err);
-        this.emit('end');
-    };
-    return gulp.src(config.scssPath + '*.scss')
-        .pipe(plumber({ errorHandler: onError }))
+    return gulp.src(config.paths.src.scss + '*.scss')
+        .pipe(plumber({ 
+            errorHandler: function(err) {
+                notify.onError({
+                    title: "SCSS Compiler Failure",
+                    subtitle: "GulpJS Failure",
+                    message: "<%= error.message %>",
+                    sound: "Beep"
+                })(err);
+                this.emit('end');
+            }
+        }))
         .pipe(sourcemaps.init())
         .pipe(sass())
         .pipe(autoprefixer())
@@ -31,12 +47,67 @@ gulp.task('sass', function() {
         .pipe(sourcemaps.write({includeContent: false}))
         .pipe(sourcemaps.init({loadMaps: true}))
         .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest(config.dest + 'css'))
+        .pipe(gulp.dest(config.paths.dest + 'css'))
         .pipe(rename({suffix: '.min'}))
         .pipe(minifycss())
-        .pipe(gulp.dest(config.dest + 'css'))
+        .pipe(gulp.dest(config.paths.dest + 'css'))
+        .pipe(browserSync.reload({ stream: true }));
 });
-gulp.task('watch', function() {
-    gulp.watch(config.scssPath + '**/*.scss', ['sass']);
+gulp.task('less', function(){
+    return gulp.src(config.paths.src.less + '*.less')
+        .pipe(plumber({ 
+            errorHandler: function(err) {
+                notify.onError({
+                    title: "LESS Compiler Failure",
+                    subtitle: "GulpJS Failure",
+                    message: "<%= error.message %>",
+                    sound: "Beep"
+                })(err);
+                this.emit('end');
+            }
+        }))
+        .pipe(sourcemaps.init())
+        .pipe(less())
+        .pipe(autoprefixer())
+        .pipe(plumber.stop())
+        .pipe(sourcemaps.write({includeContent: false}))
+        .pipe(sourcemaps.init({loadMaps: true}))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest(config.paths.dest + 'css'))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(minifycss())
+        .pipe(gulp.dest(config.paths.dest + 'css'))
+        .pipe(browserSync.reload({ stream: true }));
 });
-gulp.task('default', ['sass', 'watch']);
+gulp.task('script', function() {
+    return gulp.src(config.paths.src.scripts + '*.js')
+        .pipe(plumber({ 
+            errorHandler: function(err) {
+                notify.onError({
+                    title: "JavaScript Syntax Error",
+                    subtitle: "GulpJS Failure",
+                    message: "<%= error.message %>",
+                    sound: "Beep"
+                })(err);
+                this.emit('end');
+            } 
+        }))
+        .pipe(jshint())
+        .pipe(jshint.reporter("jshint-stylish"))
+        .pipe(jshint.reporter("fail"))
+        .pipe(concat('app.js'))
+        .pipe(gulp.dest(config.paths.dest + 'js'))
+        .pipe(rename('app.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest(config.paths.dest + 'js'))
+        .pipe(plumber.stop())
+        .pipe(browserSync.reload({ stream: true }));
+});
+gulp.task('serve', function() {
+    browserSync.init(config.serve);
+    gulp.watch(config.paths.dest + '*.html').on('change', browserSync.reload);
+    gulp.watch(config.paths.src.less + '**/*.less', ['less']);
+    gulp.watch(config.paths.src.scss + '**/*.scss', ['sass']);
+    gulp.watch(config.paths.src.scripts + '*.js', ['script']);
+});
+gulp.task('default', ['serve']);
